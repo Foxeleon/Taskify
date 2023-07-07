3eb8d2e63de210473170acff0a37956c6df6e499
Foxeleon <andreywirz@gmail.com>
2023-07-07 22:09:02 +0200

    complete todo optimisation

--- src/app/components/weekly-to-do/weekly-to-do.component.ts
+++ src/app/components/weekly-to-do/weekly-to-do.component.ts
@@ -137,6 +137,9 @@ export class WeeklyToDoComponent implements OnInit {
       case this.dailyToDosEntries.target.meaning:
         updatedWeeklyTodosArray = weeklyTodosArray.map(dailyTodo => {
           if (dailyTodo.uniqueId === uniqueId) {
+            if (dailyTodo.completePart && dailyTodo.completeLongBox && dailyTodo.completePersonalGrowth) {
+              return { ...dailyTodo, complete: true, completeTarget: true };
+            }
             return { ...dailyTodo, completeTarget: true };
           }
           return dailyTodo;
@@ -145,6 +148,9 @@ export class WeeklyToDoComponent implements OnInit {
       case this.dailyToDosEntries.part.meaning:
         updatedWeeklyTodosArray = weeklyTodosArray.map(dailyTodo => {
           if (dailyTodo.uniqueId === uniqueId) {
+            if (dailyTodo.completeTarget && dailyTodo.completeLongBox && dailyTodo.completePersonalGrowth) {
+              return { ...dailyTodo, complete: true, completePart: true };
+            }
             return { ...dailyTodo, completePart: true };
           }
           return dailyTodo;
@@ -153,6 +159,9 @@ export class WeeklyToDoComponent implements OnInit {
       case this.dailyToDosEntries.longBox.meaning:
         updatedWeeklyTodosArray = weeklyTodosArray.map(dailyTodo => {
           if (dailyTodo.uniqueId === uniqueId) {
+            if (dailyTodo.completeTarget && dailyTodo.completePart && dailyTodo.completePersonalGrowth) {
+              return { ...dailyTodo, complete: true, completeLongBox: true };
+            }
             return { ...dailyTodo, completeLongBox: true };
           }
           return dailyTodo;
@@ -161,6 +170,9 @@ export class WeeklyToDoComponent implements OnInit {
       case this.dailyToDosEntries.personalGrowth.meaning:
         updatedWeeklyTodosArray = weeklyTodosArray.map(dailyTodo => {
           if (dailyTodo.uniqueId === uniqueId) {
+            if (dailyTodo.completeTarget && dailyTodo.completePart && dailyTodo.completeLongBox) {
+              return { ...dailyTodo, complete: true, completePersonalGrowth: true };
+            }
             return { ...dailyTodo, completePersonalGrowth: true };
           }
           return dailyTodo;
@@ -169,7 +181,7 @@ export class WeeklyToDoComponent implements OnInit {
       default:
         updatedWeeklyTodosArray = weeklyTodosArray.map(dailyTodo => {
           if (dailyTodo.uniqueId === uniqueId) {
-            return { ...dailyTodo, complete: true };
+            return { ...dailyTodo, complete: !dailyTodo.complete };
           }
           return dailyTodo;
         });
