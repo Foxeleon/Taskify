d2b6e727315d9964d8103837b71c8f887aac081f
Foxeleon <andreywirz@gmail.com>
2023-08-30 23:43:00 +0200

    after restyling of component todo is no more actual

--- src/app/pipe/todo-filter.pipe.ts
+++ src/app/pipe/todo-filter.pipe.ts
@@ -8,7 +8,7 @@ export class TodoFilterPipe implements PipeTransform {
 
   transform(todos: Todo[], isCompleteList: boolean): Todo[] {
     if (!todos || isCompleteList === undefined) return todos;
-    return todos.filter(todo => todo.complete === isCompleteList);
+    return todos.filter(todo => todo.complete === isCompleteList).sort((todoA, todoB) => new Date(todoB.doneDate).getTime() - new Date(todoA.doneDate).getTime());
   }
 
 }
