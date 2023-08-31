4dd55a03694834679655bd008c4f52e4f561574a
Foxeleon <andreywirz@gmail.com>
2023-08-31 18:38:24 +0200

    refactoring

--- src/app/pipe/todo-filter.pipe.ts
+++ src/app/pipe/todo-filter.pipe.ts
@@ -8,17 +8,9 @@ export class TodoFilterPipe implements PipeTransform {
 
   transform(todos: Todo[], isCompleteList: boolean): Todo[] {
     if (!todos || isCompleteList === undefined) return todos;
-    return todos.filter(todo => todo.complete === isCompleteList).sort((todoA, todoB) => {
-      // Compare the deadline dates first
-      const deadlineDateComparison = todoA.deadline.getTime() - todoB.deadline.getTime();
-      // console.log(deadlineDateComparison);
-      if (deadlineDateComparison !== 0) {
-        return deadlineDateComparison;
-      }
-      // If deadline dates are equal, compare the creation dates
-      return todoB.creationDate.getTime() - todoA.creationDate.getTime();
-      // return (todoA.deadline.getTime() + todoA.creationDate.getTime()) - (todoB.deadline.getTime() + todoB.creationDate.getTime());
-    });
+    // Compare the deadline dates first, If deadline dates are equal, compare the creation dates
+    return todos.filter(todo => todo.complete === isCompleteList).sort((todoA, todoB) =>
+      todoA.deadline.getTime() - todoB.deadline.getTime() + todoB.creationDate.getTime() - todoA.creationDate.getTime());
   }
 
 }
